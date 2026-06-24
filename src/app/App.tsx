7904dbe35b4f6da920b4dea7b85186a7a1064c67
matt <khikmat64168@gmail.com>
2026-06-25 06:37:30 +0900

    feat:  MUI standard integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import "../css/app.css";
+import React from 'react';
+import '../css/app.css';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
 
 function App() {
-  return <div>HOME</div>;
+  // containerning o'zining props lari bor
+  return (
+    <Container maxWidth="sm">
+      <Stack flexDirection={'column'}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={'h4'}>
+            Create App on Typescript with REDUX
+          </Typography>
+        </Box>
+        <Button variant="contained">Contained</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
-
-
-
-
-
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./app/App";
-import reportWebVitals from "./reportWebVitals";
-import "./css/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './app/App';
+import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import './css/index.css';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
+//boilerplate ishlashi  redax toolkit // real dom va virtual dom
+//Material UI da 3 ta asosiy element : Container.  ,   Stack     ,    Box. U emotion enginedan foydalanadi
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
