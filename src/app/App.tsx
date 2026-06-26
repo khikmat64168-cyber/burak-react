747ba8a1f2a13ddcaec07c1efc7c8aabe4cfbd67
matt <khikmat64168@gmail.com>
2026-06-26 16:47:00 +0900

    fix: organiza header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,11 +13,14 @@
 import React from 'react';
 import '../css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, useLocation } from 'react-router-dom';
 import { HomePage } from './screens/homePage';
 import { ProductsPage } from './screens/productsPage';
 import { OrdersPage } from './screens/ordersPage';
 import { UserPage } from './screens/userPage';
+import { HomeNavbar } from './components/headers/HomeNavbar';
+import { OtherNavbar } from './components/headers/OtherNavbar';
+import { Footer } from './components/headers/footers';
 
 /**
  * ─── KOD TAHLILI ──────────────────────────────────────────────────
@@ -53,24 +56,11 @@ import { UserPage } from './screens/userPage';
  * ──────────────────────────────────────────────────────────────────
  */
 function App() {
+  const location = useLocation();
+  console.log('location:', location);
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/products">ProductsPage </Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      {location.pathname === '/' ? <HomeNavbar /> : <OtherNavbar />}
 
       {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
@@ -88,7 +78,8 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
-    </div>
+      <Footer />
+    </>
   );
 }
 
