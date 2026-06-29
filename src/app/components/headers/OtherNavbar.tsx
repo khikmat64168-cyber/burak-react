9261e62c0d7c1fb603d42d67cad2c1932742b761
matt <khikmat64168@gmail.com>
2026-06-30 04:09:52 +0900

    feat: publish other navbar header component

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,3 +1,77 @@
+import { Box, Button, Container, Stack } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
 export function OtherNavbar() {
-  return <div> OtherNavbar</div>;
+  const authMember = null;
+  return (
+    <div className="other-navbar">
+      <Container className="navbar-container">
+        <Stack className="menu">
+          <Box>
+            <NavLink to="/">
+              <img className="brand-logo" src="/icons/burak.svg" />
+            </NavLink>
+          </Box>
+          <Stack className="links">
+            <Box className={'hover-line'}>
+              <NavLink to="/">Home</NavLink>
+            </Box>
+            <Box className={'hover-line'}>
+              <NavLink to="/products" activeClassName="underline">
+                Products
+              </NavLink>
+            </Box>
+            {authMember ? (
+              <Box className={'hover-line'}>
+                <NavLink to="/orders" activeClassName="underline">
+                  Orders{' '}
+                </NavLink>
+              </Box>
+            ) : null}
+            {authMember ? (
+              <Box className={'hover-line'}>
+                <NavLink to="/member-page" activeClassName="underline">
+                  My Page{' '}
+                </NavLink>
+              </Box>
+            ) : null}
+
+            {!authMember ? (
+              <Box>
+                <Button variant="contained" className="login-buttons">
+                  Login
+                </Button>
+              </Box>
+            ) : (
+              <img
+                className="user-avatar"
+                src={'/icons/default-user.svg'}
+                aria-haspopup={'true'}
+              />
+            )}
+            <Box>
+              <NavLink to="/help">Help</NavLink>
+            </Box>
+            {/* BASKET*/}
+          </Stack>
+        </Stack>
+        <Stack className={'header-frame'}>
+          <Stack className="detail">
+            <Box className={'head-main-text'}>
+              World's most delicious cusine
+            </Box>
+            <Box className={'wel-text'}> The choice , not just a choice </Box>
+            <Box className={'service-text'}> 24 hour service</Box>
+            <Box className={'signup'}>
+              {!authMember ? (
+                <Button variant={'contained'} className={'signup-button'}>
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
